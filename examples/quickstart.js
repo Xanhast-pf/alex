0f96fcbaf3fcddef780b30aff48c5e6c9ea1ac1d
Xanhast <jbouquet47@gmail.com>
2016-11-30 11:57:41 -0500

    work in progress. Plugin weather API

--- examples/quickstart.js
+++ examples/quickstart.js
@@ -1,62 +1,132 @@
+// @flow
 'use strict';
+
+import weather from 'weather-js';
 
 let Wit = null;
 let interactive = null;
 try {
   // if running from repo
-  Wit = require('../').Wit;
-  interactive = require('../').interactive;
+    Wit = require('../').Wit;
+    interactive = require('../').interactive;
 } catch (e) {
-  Wit = require('node-wit').Wit;
-  interactive = require('node-wit').interactive;
+    Wit = require('node-wit').Wit;
+    interactive = require('node-wit').interactive;
 }
 
-const accessToken = (() => {
-  if (process.argv.length !== 3) {
-    console.log('usage: node examples/quickstart.js <wit-access-token>');
-    process.exit(1);
-  }
-  return process.argv[2];
-})();
+// const accessToken = (() => {
+//   if (process.argv.length !== 3) {
+//     console.log('usage: node examples/quickstart.js <wit-access-token>');
+//     process.exit(1);
+//   }
+//   return process.argv[2];
+// })();
+
+// const accessToken = 'NES67P6A4AVRL557TOCTZBLFL2UUINC4'; // SUNNY
+const accessToken = '4737MR7N7PQAEAXHGHPBDGCWNC6LUCKN'; // ALEX
 
 // Quickstart example
 // See https://wit.ai/ar7hur/quickstart
 
 const firstEntityValue = (entities, entity) => {
-  const val = entities && entities[entity] &&
+    const val = entities && entities[entity] &&
     Array.isArray(entities[entity]) &&
     entities[entity].length > 0 &&
     entities[entity][0].value
   ;
-  if (!val) {
-    return null;
-  }
-  return typeof val === 'object' ? val.value : val;
+    if (!val) {
+        return null;
+    }
+    return typeof val === 'object' ? val.value : val;
 };
 
 const actions = {
-  send(request, response) {
-    const {sessionId, context, entities} = request;
-    const {text, quickreplies} = response;
-    return new Promise(function(resolve, reject) {
-      console.log('sending...', JSON.stringify(response));
-      return resolve();
-    });
-  },
-  getForecast({context, entities}) {
-    return new Promise(function(resolve, reject) {
-      var location = firstEntityValue(entities, 'location')
-      if (location) {
-        context.forecast = 'sunny in ' + location; // we should call a weather API here
-        delete context.missingLocation;
-      } else {
-        context.missingLocation = true;
-        delete context.forecast;
-      }
-      return resolve(context);
-    });
-  },
+    send(request, response) {
+        const { sessionId, context, entities } = request;
+        const { text, quickreplies } = response;
+        return new Promise((resolve, reject) => {
+            console.log('Alex: ', JSON.stringify(response));
+            return resolve();
+        });
+    },
+    // getForecast({ context, entities }) {
+    //     console.log('=================================================');
+    //     console.log('context : ', context);
+    //     console.log('entities : ', entities);
+    //     return new Promise((resolve, reject) => {
+    //         var location = firstEntityValue(entities, 'location');
+    //         console.log('LOCATION: ', location);
+    //         if (location) {
+    //             weather.find({ search: location, degreeType: 'C' }, (err, result) => {
+    //                 if (err) {
+    //                     context.forecast = 'Sorry I couldn\'t find it...';
+    //                 } else {
+    //                     context.forecast = `Location : ${ result[0].current.observationpoint } | Sky : ${ result[0].current.skytext } | Temperature : ${ result[0].current.temperature } C`;
+    //                 }
+    //             });
+    //
+    //             delete context.missingLocation;
+    //         } else {
+    //             context.missingLocation = true;
+    //             delete context.forecast;
+    //         }
+    //         console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+    //         return resolve(context);
+    //     });
+    // },
+    getForecast({ context, entities }) {
+        console.log('=================================================');
+        console.log('context : ', context);
+        console.log('entities : ', entities);
+        return new Promise((resolve, reject) => {
+            const location = firstEntityValue(entities, 'location');
+            if (location) {
+                new Promise((resolve, reject) => {
+                    weather.find({ search: location, degreeType: 'C' }, (err, result) => {
+                        const res = (err)
+                            ? 'Sorry I couldn\'t find it...'
+                            : `Location : ${ result[0].current.observationpoint } | Sky : ${ result[0].current.skytext } | Temperature : ${ result[0].current.temperature } C`;
+                        resolve(res);
+                    });
+                }).then((val) => {
+                    context.forecast = val;
+                    delete context.missingLocation;
+                    console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+                    return resolve(context);
+                });
+            } else {
+                context.missingLocation = true;
+                delete context.forecast;
+                console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+                return resolve(context);
+            }
+        });
+    },
+    Greet({ context, entities }) {
+        return new Promise((resolve, reject) => {
+            const intent = firstEntityValue(entities, 'intent');
+            if (intent === 'greetings') {
+                context.greetings = 'Greetings Master! It would be an honor to serve you today!';
+            } else {
+                delete context.greetings;
+            }
+            return resolve(context);
+        });
+    },
+    findTheater({ context, entities }) {
+        return new Promise((resolve, reject) => {
+            const intent = firstEntityValue(entities, 'findTheater');
+            const movie = firstEntityValue(entities, 'movie');
+
+            if (intent === 'findTheater') {
+                context.theater = 'Looking for availability for : ' + movie;
+            } else {
+                delete context.theater;
+            }
+            return resolve(context);
+        });
+    }
 };
 
-const client = new Wit({accessToken, actions});
+const client = new Wit({ accessToken, actions });
 interactive(client);
