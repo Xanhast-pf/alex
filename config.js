16b408996f1b0956ba3aef58206044a454d535c5
Xanhast <jbouquet47@gmail.com>
2016-12-06 16:11:56 -0500

    bot now on messenger for testing purposes

--- config.js
+++ config.js
@@ -1,17 +1,26 @@
+// @flow
 // Webserver parameter
-export const PORT = process.env.PORT || 8445;
+const PORT = process.env.PORT || 8445;
 
 // Wit.ai parameters
-export const WIT_TOKEN = process.env.WIT_TOKEN;
+const WIT_TOKEN = process.env.WIT_TOKEN;
 
 // Messenger API parameters
-export const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 if (!FB_PAGE_TOKEN) {
     throw new Error('missing FB_PAGE_TOKEN');
 }
-export const FB_APP_SECRET = process.env.FB_APP_SECRET;
+const FB_APP_SECRET = process.env.FB_APP_SECRET;
 if (!FB_APP_SECRET) {
     throw new Error('missing FB_APP_SECRET');
 }
 
-export const FB_VERIFY_TOKEN = 'jogogoalex';
+const FB_VERIFY_TOKEN = 'jogogoalex';
+
+export {
+    PORT,
+    WIT_TOKEN,
+    FB_VERIFY_TOKEN,
+    FB_APP_SECRET,
+    FB_PAGE_TOKEN
+};
