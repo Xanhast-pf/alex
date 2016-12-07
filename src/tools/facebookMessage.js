c16178f6d012def3015f0620c6ec2a37d27cb045
Xanhast <jbouquet47@gmail.com>
2016-12-07 17:05:54 -0500

    work in progress

--- src/tools/facebookMessage.js
+++ src/tools/facebookMessage.js
@@ -24,57 +24,6 @@ const fbMessage = (id: string, message: Object) => {
       });
 };
 
-const fbGenericTemplate = (id: string, imageUrl: string) => {
-    const body = JSON.stringify({
-        recipient: { id },
-        imageUrl,
-        message: {
-            attachment: {
-                type: 'template',
-                payload: {
-                    template_type: 'generic',
-                    elements: [
-                        {
-                            title: 'Welcome to Peter\'s Hats',
-                            item_url: 'https://petersfancybrownhats.com',
-                            image_url: 'https://petersfancybrownhats.com/company_image.png',
-                            subtitle: 'We\'ve got the right hat for everyone.',
-                            buttons: [
-                                {
-                                    type: 'web_url',
-                                    url: 'https://petersfancybrownhats.com',
-                                    title: 'View Website'
-                                },
-                                {
-                                    type: 'postback',
-                                    title: 'Start Chatting',
-                                    payload: 'DEVELOPER_DEFINED_PAYLOAD'
-                                }
-                            ]
-                        }
-                    ]
-                }
-            }
-        }
-    });
-
-    const qs = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
-
-    return fetch('https://graph.facebook.com/me/messages?' + qs, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body
-    })
-      .then(rsp => rsp.json())
-      .then((json) => {
-          if (json.error && json.error.message) {
-              throw new Error(json.error.message);
-          }
-          return json;
-      });
-};
-
 export {
-    fbMessage,
-    fbGenericTemplate
+    fbMessage
 };
